ecca6bf024e4100d097f421d2189703384c40902
Flavio Medeiros <smedeiros.flavio@gmail.com>
2024-09-16 22:58:36 -0300

    Adicionado o readme e a programação de 6 botoes

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# sim-racing
+# Sim racing
+
+## O que é?
+
+Sim racing é um tipo de competição de automobilismo virtual, onde os pilotos competem em corridas de carros de corrida em simuladores de corrida de computador. O sim racing é uma forma de simulação de corrida de automóveis que é usada para treinar pilotos de corrida, para fornecer uma experiência de corrida realista e para fornecer entretenimento para os jogadores de simulação de corrida.
+
+## Objetivo do repositorio
+
+Este projeto tem como o objetivo a criação de um simulador de corrida de carros.
+
+## Tecnologias utilizadas
+
+- [FFBeast](https://ffbeast.github.io/)
+  - Utilizado para a criação do volante + force feedback
+- [ESP32-BLE-Gamepad](https://github.com/lemmingDev/ESP32-BLE-Gamepad/)
+  - Biblioteca utilizada no ESP32
+- [SimHub](https://www.simhubdash.com/)
+  - Utilizado para a criação do painel de instrumentos
+
+## Hardware utilizados
+
+- Motor de hoverboard
+  - Utilizado para o force feedback
+- [ODESC 4.2](https://pt.aliexpress.com/item/1005005763345874.html)
+  - Utilizado para controlar o motor de hoverboard junto ao FFBEAST
+- [ESP32](https://www.espressif.com/en/products/socs/esp32)
+  - Utilizado para a comunicação entre o volante e o computador
+- [ENDER 3 v3 KE](https://www.creality.com/products/creality-ender-3-v3-ke)
+  - Impressora 3D utilizado para a impressão 3D de peças
+- [12 Botão momentaneo](https://pt.aliexpress.com/item/1005005904666959.html?spm=a2g0o.order_list.order_list_main.28.28a6caa4CzDX54&gatewayAdapt=glo2bra)
+  - Botões utilizados para os controles do volante
+- [2 mancal SK16](https://bulkman3d.aliexpress.com/store/1752067?spm=a2g0o.order_list.order_list_main.50.28a6caa4CzDX54)
+- [Resistor de frenagem 50w 10ohm](https://pt.aliexpress.com/item/1005006825516125.html?spm=a2g0o.order_list.order_list_main.11.28a6caa4CzDX54&gatewayAdapt=glo2bra)
+- [Fonte 12v 30a](https://produto.mercadolivre.com.br/MLB-3732855248-fonte-24v-30a-720w-chaveada-estabilizada-bivolt-cabo-_JM)
+- [Encoder Rotativo 1000ppr](https://pt.aliexpress.com/item/1005005779039462.html?spm=a2g0o.order_list.order_list_main.77.28a6caa4CzDX54&gatewayAdapt=glo2bra)
+- [Sensor hall 49e](https://pt.aliexpress.com/item/32852539663.html?spm=a2g0o.order_list.order_list_main.47.28a6caa4CzDX54&gatewayAdapt=glo2bra)
+- [2 Cooler 24v](https://pt.aliexpress.com/item/1005002468157849.html?spm=a2g0o.order_list.order_list_main.23.28a6caa4CzDX54&gatewayAdapt=glo2bra)
+- [2 micro switch](https://pt.aliexpress.com/item/1005006085607858.html?spm=a2g0o.order_list.order_list_main.89.28a6caa4CzDX54&gatewayAdapt=glo2bra)
+
+## Referencias
+
+- [FFBeast](https://ffbeast.github.io/docs/en/wheel.html)
+- [A Chave Virou](https://www.youtube.com/watch?v=a1Q18tryYvI)
+  - Excelente video sobre a construção do volante + force feedback
+
+## Esquemas eletricos
+
+@todo
---
+++ volante_botoes.ino
@@ -0,0 +1,98 @@
+/*
+ * This code programs a number of pins on an ESP32 as buttons on a BLE gamepad
+ *
+ * It uses arrays to cut down on code
+ *
+ * Uses the Bounce2 library to debounce all buttons
+ *
+ * Uses the rose/fell states of the Bounce instance to track button states
+ *
+ * Before using, adjust the numOfButtons, buttonPins and physicalButtons to suit your senario
+ *
+ */
+
+#define BOUNCE_WITH_PROMPT_DETECTION // Make button state changes available immediately
+
+#include <Arduino.h>
+#include <Bounce2.h>    // https://github.com/thomasfredericks/Bounce2
+#include <BleGamepad.h> // https://github.com/lemmingDev/ESP32-BLE-Gamepad
+
+#define numOfButtons 6
+#define numOfHatSwitches 0
+#define enableX false
+#define enableY false
+#define enableZ false
+#define enableRX false
+#define enableRY false
+#define enableRZ false
+#define enableSlider1 false
+#define enableSlider2 false
+#define enableRudder false
+#define enableThrottle false
+#define enableAccelerator false
+#define enableBrake false
+#define enableSteering false
+
+Bounce debouncers[numOfButtons];
+BleGamepad bleGamepad("Volante", "medeiroz", 100); // Name, Manufacturer, HID Report length
+
+byte buttonPins[numOfButtons] = {5, 16, 15, 26, 14, 13};
+byte physicalButtons[numOfButtons] = {1, 2, 3, 4, 5, 6};
+
+void setup()
+{
+  for (byte currentPinIndex = 0; currentPinIndex < numOfButtons; currentPinIndex++)
+  {
+    pinMode(buttonPins[currentPinIndex], INPUT_PULLUP);
+
+    debouncers[currentPinIndex] = Bounce();
+    debouncers[currentPinIndex].attach(buttonPins[currentPinIndex]); // After setting up the button, setup the Bounce instance :
+    debouncers[currentPinIndex].interval(5);
+  }
+
+  BleGamepadConfiguration bleGamepadConfig;
+  bleGamepadConfig.setButtonCount(numOfButtons);
+  bleGamepadConfig.setAutoReport(false);
+  bleGamepad.begin(&bleGamepadConfig);
+
+  // changing bleGamepadConfig after the begin function has no effect, unless you call the begin function again
+
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  if (bleGamepad.isConnected())
+  {
+    bool sendReport = false;
+
+    for (byte currentIndex = 0; currentIndex < numOfButtons; currentIndex++)
+    {
+      debouncers[currentIndex].update();
+
+      if (debouncers[currentIndex].fell())
+      {
+        bleGamepad.press(physicalButtons[currentIndex]);
+        sendReport = true;
+        Serial.print("Button ");
+        Serial.print(physicalButtons[currentIndex]);
+        Serial.println(" pushed.");
+      }
+      else if (debouncers[currentIndex].rose())
+      {
+        bleGamepad.release(physicalButtons[currentIndex]);
+        sendReport = true;
+        Serial.print("Button ");
+        Serial.print(physicalButtons[currentIndex]);
+        Serial.println(" released.");
+      }
+    }
+
+    if (sendReport)
+    {
+      bleGamepad.sendReport();
+    }
+
+    // delay(20);	// (Un)comment to remove/add delay between loops
+  }
+}
