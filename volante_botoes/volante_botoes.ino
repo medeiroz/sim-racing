b4d989b665bc8a6ae6b9526713f022e4ab64f0d9
Flavio Medeiros <smedeiros.flavio@gmail.com>
2024-11-09 18:45:00 -0300

    Ajustes de padrao de codigo

--- volante_botoes/volante_botoes.ino
+++ volante_botoes/volante_botoes.ino
@@ -1,53 +1,53 @@
 #include <Arduino.h>
 #include <Bounce2.h>    // https://github.com/thomasfredericks/Bounce2
 #include <BleGamepad.h> // https://github.com/lemmingDev/ESP32-BLE-Gamepad
-#include <TickTwo.h> // https://github.com/sstaub/TickTwo
+#include <TickTwo.h>    // https://github.com/sstaub/TickTwo
 
 #define BOUNCE_WITH_PROMPT_DETECTION // Make button state changes available immediately
 
 void batteryLoop();
 
-#define PEDALS_SIMULATION false;
+#define PEDALS_SIMULATION false
 
-#define ACCELERATION_BUTTON 99  // Defina o botão 7 como acelerador
-#define BRAKE_BUTTON 100         // Defina o botão 1 como freio
+#define ACCELERATION_BUTTON 7 // Define button 7 as accelerator
+#define BRAKE_BUTTON 1        // Define button 1 as brake
 
-#define BATTERY_PIN 34 // Pino ADC conectado ao divisor de tensão
-#define C_LED_PIN 14 // led indicativo que esta ligado
+#define BATTERY_PIN 34 // ADC pin connected to the voltage divider
+#define C_LED_PIN 14   // LED indicating that it is on
 
-#define numOfButtons 14
-#define numOfHatSwitches 0
-#define enableX false
-#define enableY false
-#define enableZ false
-#define enableRX false
-#define enableRY false
-#define enableRZ false
-#define enableSlider1 false
-#define enableSlider2 false
-#define enableRudder false
-#define enableThrottle false
-#define enableAccelerator false
-#define enableBrake false
-#define enableSteering false
+#define NUM_OF_BUTTONS 14
+#define NUM_OF_HAT_SWITCHES 0
+#define ENABLE_X false
+#define ENABLE_Y false
+#define ENABLE_Z false
+#define ENABLE_RX false
+#define ENABLE_RY false
+#define ENABLE_RZ false
+#define ENABLE_SLIDER_1 false
+#define ENABLE_SLIDER_2 false
+#define ENABLE_RUDDER false
+#define ENABLE_THROTTLE false
+#define ENABLE_ACCELERATOR false
+#define ENABLE_BRAKE false
+#define ENABLE_STEERING false
 
 #define MIN_PEDALS -32767
 #define MAX_PEDALS 32767
 
-Bounce debouncers[numOfButtons];
-BleGamepad bleGamepad("Volante F1", "medeiroz", 100);
+Bounce debouncers[NUM_OF_BUTTONS];
+BleGamepad bleGamepad("F1 Steering Wheel", "medeiroz", 100);
 
-byte buttonPins[numOfButtons] = {
-    21, 19, 18, 5, 17, 16,   // Left Buttons
+byte buttonPins[NUM_OF_BUTTONS] = {
+    21, 19, 18, 5, 17, 16,  // Left Buttons
     13, 12, 32, 33, 25, 26, // Right Buttons
     23,                     // Left Shifter
-    27,                     // Right Shifter
+    27                      // Right Shifter
 };
-byte physicalButtons[numOfButtons] = {
-    1, 2, 3, 4, 5, 6,             // Left Buttons 1 breaks
-    7, 8, 9, 10, 11, 12,          // Right Buttons 7 acceleration
-    13,                           // Left Shifter
-    14,                           // Right Shifter
+byte physicalButtons[NUM_OF_BUTTONS] = {
+    1, 2, 3, 4, 5, 6,    // Left Buttons 1 breaks
+    7, 8, 9, 10, 11, 12, // Right Buttons 7 acceleration
+    13,                  // Left Shifter
+    14                   // Right Shifter
 };
 
 int acceleration = MIN_PEDALS;
@@ -67,35 +67,30 @@ TickTwo batteryTicker(batteryLoop, 1000 * 10); // 10s
 void setup()
 {
   pinMode(C_LED_PIN, OUTPUT);
-  // pinMode(LED_BUILTIN, OUTPUT);
-  // digitalWrite(LED_BUILTIN, LOW);
 
   batteryTicker.start();
 
-  for (byte currentPinIndex = 0; currentPinIndex < numOfButtons; currentPinIndex++)
+  for (byte currentPinIndex = 0; currentPinIndex < NUM_OF_BUTTONS; currentPinIndex++)
   {
     pinMode(buttonPins[currentPinIndex], INPUT_PULLUP);
 
     debouncers[currentPinIndex] = Bounce();
-    debouncers[currentPinIndex].attach(buttonPins[currentPinIndex]); // After setting up the button, setup the Bounce instance :
+    debouncers[currentPinIndex].attach(buttonPins[currentPinIndex]); // After setting up the button, set up the Bounce instance:
     debouncers[currentPinIndex].interval(5);
   }
 
   BleGamepadConfiguration bleGamepadConfig;
   bleGamepadConfig.setAutoReport(false);
   bleGamepadConfig.setControllerType(CONTROLLER_TYPE_GAMEPAD);
-  bleGamepadConfig.setButtonCount(numOfButtons);
-  bleGamepadConfig.setWhichAxes(enableX, enableY, enableZ, enableRX, enableRY, enableRZ, enableSlider1, enableSlider2);
-  bleGamepadConfig.setWhichSimulationControls(enableRudder, enableThrottle, enableAccelerator, enableBrake, enableSteering);
-  bleGamepadConfig.setHatSwitchCount(numOfHatSwitches);
+  bleGamepadConfig.setButtonCount(NUM_OF_BUTTONS);
+  bleGamepadConfig.setWhichAxes(ENABLE_X, ENABLE_Y, ENABLE_Z, ENABLE_RX, ENABLE_RY, ENABLE_RZ, ENABLE_SLIDER_1, ENABLE_SLIDER_2);
+  bleGamepadConfig.setWhichSimulationControls(ENABLE_RUDDER, ENABLE_THROTTLE, ENABLE_ACCELERATOR, ENABLE_BRAKE, ENABLE_STEERING);
+  bleGamepadConfig.setHatSwitchCount(NUM_OF_HAT_SWITCHES);
 
   bleGamepadConfig.setSimulationMin(MIN_PEDALS);
   bleGamepadConfig.setSimulationMax(MAX_PEDALS);
 
   bleGamepad.begin(&bleGamepadConfig);
-
-
-  // changing bleGamepadConfig after the begin function has no effect, unless you call the begin function again
 
   Serial.begin(115200);
 }
@@ -113,63 +108,66 @@ void loop()
   delay(20);
 }
 
-void gamepadLoop() {
-  
+void gamepadLoop()
+{
   bool sendReport = false;
 
-  for (byte currentIndex = 0; currentIndex < numOfButtons; currentIndex++)
+  for (byte currentIndex = 0; currentIndex < NUM_OF_BUTTONS; currentIndex++)
   {
     debouncers[currentIndex].update();
 
     if (PEDALS_SIMULATION && physicalButtons[currentIndex] == ACCELERATION_BUTTON)
     {
-      if (debouncers[currentIndex].fell()) {
-        Serial.println("Acelerador pressionado");
+      if (debouncers[currentIndex].fell())
+      {
+        Serial.println("Accelerator pressed");
         sendReport = true;
-        acelerar();
+        accelerate();
       }
-      if (debouncers[currentIndex].rose()) {
-        Serial.println("Acelerador Solto");
+      if (debouncers[currentIndex].rose())
+      {
+        Serial.println("Accelerator released");
         sendReport = true;
-        desacelerar();
+        decelerate();
       }
     }
 
     if (PEDALS_SIMULATION && physicalButtons[currentIndex] == BRAKE_BUTTON)
     {
-      if (debouncers[currentIndex].fell()) {
-        Serial.println("Freio pressionado");
+      if (debouncers[currentIndex].fell())
+      {
+        Serial.println("Brake pressed");
         sendReport = true;
-        freiar();
+        brakeAction();
       }
-      if (debouncers[currentIndex].rose()) {
-        Serial.println("Freio solto");
+      if (debouncers[currentIndex].rose())
+      {
+        Serial.println("Brake released");
         sendReport = true;
-        desfreiar();
+        releaseBrake();
       }
     }
 
-    // OUTROS BOTOES
-    if (physicalButtons[currentIndex] != ACCELERATION_BUTTON && physicalButtons[currentIndex] != BRAKE_BUTTON)
+    // OTHER BUTTONS
+    if (!PEDALS_SIMULATION || (physicalButtons[currentIndex] != ACCELERATION_BUTTON && physicalButtons[currentIndex] != BRAKE_BUTTON))
     {
-        if (debouncers[currentIndex].fell())
-        {
-          bleGamepad.press(physicalButtons[currentIndex]);
-          sendReport = true;
-          Serial.print("Button ");
-          Serial.print(physicalButtons[currentIndex]);
-          Serial.println(" pressionado.");
-        }
-        
-        
-        if (debouncers[currentIndex].rose())
-        {
-          bleGamepad.release(physicalButtons[currentIndex]);
-          sendReport = true;
-          Serial.print("Button ");
-          Serial.print(physicalButtons[currentIndex]);
-          Serial.println(" liberado.");
-        }
+      if (debouncers[currentIndex].fell())
+      {
+        bleGamepad.press(physicalButtons[currentIndex]);
+        sendReport = true;
+        Serial.print("Button ");
+        Serial.print(physicalButtons[currentIndex]);
+        Serial.println(" pressed.");
+      }
+
+      if (debouncers[currentIndex].rose())
+      {
+        bleGamepad.release(physicalButtons[currentIndex]);
+        sendReport = true;
+        Serial.print("Button ");
+        Serial.print(physicalButtons[currentIndex]);
+        Serial.println(" released.");
+      }
     }
   }
 
@@ -177,13 +175,15 @@ void gamepadLoop() {
   {
     report();
   }
-  
+
   delay(20);
 }
 
 // For PEDALS_SIMULATION
-void acelerar() {
-  for (int i = acceleration; i < MAX_PEDALS; i += 2048) {
+void accelerate()
+{
+  for (int i = acceleration; i < MAX_PEDALS; i += 2048)
+  {
     acceleration = i;
     report();
     delay(2);
@@ -193,8 +193,10 @@ void acelerar() {
 }
 
 // for PEDALS_SIMULATION
-void desacelerar() {
-  for (int i = acceleration; i > MIN_PEDALS; i -= 2048) {
+void decelerate()
+{
+  for (int i = acceleration; i > MIN_PEDALS; i -= 2048)
+  {
     acceleration = i;
     report();
     delay(2);
@@ -204,8 +206,10 @@ void desacelerar() {
 }
 
 // for PEDALS_SIMULATION
-void freiar() {
-  for (int i = brake; i < MAX_PEDALS; i += 1024) {
+void brakeAction()
+{
+  for (int i = brake; i < MAX_PEDALS; i += 1024)
+  {
     brake = i;
     report();
     delay(2);
@@ -215,8 +219,10 @@ void freiar() {
 }
 
 // for PEDALS_SIMULATION
-void desfreiar() {
-  for (int i = brake; i > MIN_PEDALS; i -= 1024) {
+void releaseBrake()
+{
+  for (int i = brake; i > MIN_PEDALS; i -= 1024)
+  {
     brake = i;
     report();
     delay(2);
@@ -225,43 +231,41 @@ void desfreiar() {
   report();
 }
 
-void report() {
+void report()
+{
   bleGamepad.sendReport();
 
-  if (PEDALS_SIMULATION) {
+  if (PEDALS_SIMULATION)
+  {
     bleGamepad.setAccelerator(acceleration);
-    // bleGamepad.setThrottle(acceleration);
     bleGamepad.setBrake(brake);
     Serial.print("Report: ");
-    Serial.print(" Acelerador: ");
+    Serial.print(" Accelerator: ");
     Serial.print(acceleration);
-    Serial.print(" | Freio: ");
+    Serial.print(" | Brake: ");
     Serial.print(brake);
     Serial.println("");
   }
 }
 
-void batteryLoop() {
-  // Leitura da tensão no pino ADC
+void batteryLoop()
+{
   int adcValue = analogRead(BATTERY_PIN);
+  float voltage = adcValue * (3.3 / 4095.0); // 3.3V is the reference voltage of the ADC
+  batteryVoltage = voltage * 2;              // Battery voltage is double the read voltage (voltage divider with R1 = R2 = 150Ω)
 
-  // Converta o valor ADC para a tensão lida no divisor de tensão
-  float voltage = adcValue * (3.3 / 4095.0); // 3.3V é a tensão de referência do ADC
+  batteryPercentage = (batteryVoltage - 3.0) / (4.2 - 3.0) * 100.0; // Battery percentage calculation based on 3.0V to 4.2V range
 
-  // A tensão da bateria é o dobro da tensão lida (divisor de tensão com R1 = R2 = 150Ω)
-  batteryVoltage = voltage * 2;
-
-  // Cálculo da porcentagem da bateria (com base nos valores de 3.0V a 4.2V)
-  batteryPercentage = (batteryVoltage - 3.0) / (4.2 - 3.0) * 100.0;
-
-  // Limite a porcentagem entre 0 e 100
-  if (batteryPercentage > 100) batteryPercentage = 100;
-  if (batteryPercentage < 0) batteryPercentage = 0;
+  if (batteryPercentage > 100)
+    batteryPercentage = 100;
+  if (batteryPercentage < 0)
+    batteryPercentage = 0;
 
   batteryReport();
 }
 
-void batteryReport() {
+void batteryReport()
+{
   bleGamepad.setBatteryLevel(batteryPercentage);
 
   Serial.print("Battery Voltage: ");
@@ -271,26 +275,24 @@ void batteryReport() {
   Serial.print(batteryPercentage);
   Serial.print(" %");
   Serial.print(", ADC value: ");
-  Serial.print(adcValue);
+  Serial.print(analogRead(BATTERY_PIN));
   Serial.println("");
 }
 
-void ledLoop() {
+void ledLoop()
+{
   unsigned long currentMillis = millis();
-
   unsigned long actualLedInterval = ledState == LOW ? ledIntervalLow : ledIntervalHigh;
 
-  if (currentMillis - ledPreviousMillis >= actualLedInterval) {
+  if (currentMillis - ledPreviousMillis >= actualLedInterval)
+  {
     ledPreviousMillis = currentMillis;
-
     ledState = ledState == LOW ? HIGH : LOW;
     digitalWrite(C_LED_PIN, ledState);
 
-    if (ledState == LOW) {
+    if (ledState == LOW)
+    {
       ledIntervalLow = bleGamepad.isConnected() && ledIntervalLow == ledInterval ? ledIntervalHigh : ledInterval;
     }
   }
 }
-
-
-
